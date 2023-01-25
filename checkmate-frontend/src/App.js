f14e865cc305418d541bbd5cbebceff70f77a8aa
abby <abbychoi97@gmail.com>
2023-01-25 00:16:44 -0800

    used async functions for chained functions

--- checkmate-frontend/src/App.js
+++ checkmate-frontend/src/App.js
@@ -8,57 +8,55 @@ function App() {
     interactions: [],
   });
 
-  const getRxCUIs = (formData) => {
+  const getRxCUIs = async (formData) => {
     console.log("getRxCUIs called");
     // console.log(formData);
     const newRxCUIs = [];
     for (const drugInfo of formData) {
-      axios
-        .get(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drugInfo.drug}`)
-        .then((response) => {
-          console.log(response.data);
-          console.log(Object.keys(response.data.idGroup).length);
-          if (Object.keys(response.data.idGroup).length > 0) {
-            console.log(response.data.idGroup.rxnormId[0]);
-            newRxCUIs.push(response.data.idGroup.rxnormId[0]);
-          } else {
-            const errorMessage = `${drugInfo.drug} does not exist.`;
-            // console.log(errorMessage);
-            alert(errorMessage);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      try {
+        let response = await axios.get(
+          `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drugInfo.drug}`
+        );
+        console.log(response.data);
+        console.log(Object.keys(response.data.idGroup).length);
+        if (Object.keys(response.data.idGroup).length > 0) {
+          console.log(response.data.idGroup.rxnormId[0]);
+          newRxCUIs.push(response.data.idGroup.rxnormId[0]);
+        }
+      } catch (error) {
+        const errorMessage = `${drugInfo.drug} does not exist.`;
+        // console.log(errorMessage);
+        alert(errorMessage);
+      }
     }
     const newCurrentData = { ...currentData, rxCUIs: newRxCUIs };
     console.log(newCurrentData);
     console.log(newRxCUIs);
     setCurrentData(newCurrentData);
+    return newRxCUIs;
   };
 
-  const getInteractions = (formData) => {
+  const getInteractions = async (formData) => {
     console.log("getInteractions called");
     const newInteractions = [];
-    getRxCUIs(formData)
-      .then(() => {
-        const rxCUIsCode = currentData.rxCUIs.join("+");
-        let interactionURL = `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${rxCUIsCode}`;
-        axios.get(interactionURL).then((response) => {
-          if ("fullInteractionTypeGroup" in response.data) {
-            const interactions = response.data.fullInteractionTypeGroup.length;
-            console.log(interactions);
-            console.log(response.data);
-            newInteractions.push(interactions);
-          } else {
-            const disclaimer = "disclaimer :)";
-            console.log(disclaimer);
-          }
-        });
-      })
-      .catch((error) => {
-        console.log(error);
+    let rxCUICodes = await getRxCUIs(formData);
+    try {
+      const rxCUIsCode = rxCUICodes.join("+");
+      let interactionURL = `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${rxCUIsCode}`;
+      axios.get(interactionURL).then((response) => {
+        if ("fullInteractionTypeGroup" in response.data) {
+          const interactions = response.data.fullInteractionTypeGroup.length;
+          console.log(interactions);
+          console.log(response.data);
+          newInteractions.push(interactions);
+        } else {
+          const disclaimer = "disclaimer :)";
+          console.log(disclaimer);
+        }
       });
+    } catch (error) {
+      console.log(error);
+    }
     const newCurrentData = { ...currentData, interactions: newInteractions };
     console.log(newCurrentData);
     setCurrentData(newCurrentData);
