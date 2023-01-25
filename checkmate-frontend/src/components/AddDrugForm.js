1c90bd5770a06f04cc843c8966a438adc61b8f42
abby <abbychoi97@gmail.com>
2023-01-25 03:15:51 -0800

    added autocomplete for drug names

--- checkmate-frontend/src/components/AddDrugForm.js
+++ checkmate-frontend/src/components/AddDrugForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 const INITIAL_FORM_DATA = {
   drug: "",
@@ -9,14 +10,31 @@ const INITIAL_FORM_DATA = {
 
 const AddDrugForm = ({ getInteractions, getRxCUIs }) => {
   const [formData, setFormData] = useState([INITIAL_FORM_DATA]);
+  const [suggestions, setSuggestions] = useState([]);
   // console.log(formData);
 
-  const handleChange = (i, e) => {
+  const handleChange = async (i, e) => {
     let newFormData = [...formData];
     // console.log(e);
     // console.log(e.target);
     newFormData[i][e.target.name] = e.target.value;
     setFormData(newFormData);
+
+    let suggestionsData = [];
+    let response = await axios.get(
+      `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=${e.target.value}&ef=STRENGTHS_AND_FORMS`
+    );
+    console.log(response.data);
+    try {
+      let suggestions = response.data[1];
+      for (let idx in suggestions) {
+        suggestionsData.push(suggestions[idx]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(suggestionsData);
+    setSuggestions(suggestionsData);
   };
 
   const addFormFields = () => {
@@ -31,7 +49,13 @@ const AddDrugForm = ({ getInteractions, getRxCUIs }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    getInteractions(formData);
+    const newFormData = [];
+    for (let idx in formData) {
+      const drugName = formData[idx]["drug"].split(" (")[0].toLowerCase();
+      console.log(`drugName= ${drugName}`);
+      newFormData.push({ ...formData[idx], drug: drugName });
+    }
+    getInteractions(newFormData);
     // alert(JSON.stringify(formData));
   };
 
@@ -42,13 +66,19 @@ const AddDrugForm = ({ getInteractions, getRxCUIs }) => {
           <div className="inline-form" key={index}>
             <label htmlFor="drug"> Drug Name:</label>
             <input
-              type="text"
+              type="search"
+              list="drug-name-suggestions"
               id="drug"
               name="drug"
               value={element.drug || ""}
               placeholder="drug name"
               onChange={(e) => handleChange(index, e)}
             />
+            <datalist id="drug-name-suggestions">
+              {suggestions.map((suggestion, idx) => {
+                return <option key={idx} value={suggestion}></option>;
+              })}
+            </datalist>
             <label htmlFor="dose"> Dose:</label>
             <input
               type="text"
