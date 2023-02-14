d46079a5b11e8a9e0ab905f38cd33a173e08f1ee
abby <abbychoi97@gmail.com>
2023-02-13 22:33:30 -0800

    updating getinteractions function

--- checkmate-frontend/src/App.js
+++ checkmate-frontend/src/App.js
@@ -114,53 +114,51 @@ function App() {
   };
 
   const getInteractions = async (formData, type) => {
-    if (formData[0].drug || profileData.rxCUIs[0]) {
-      console.log("getInteractions called");
-      // console.log(formData);
-      const rxCUICodes = [];
-      const drugs = [];
-      const doses = [];
-      const frequencies = [];
-      for (let idx in formData) {
-        rxCUICodes.push(formData[idx].rxCUI);
-        let drugName =
-          formData[idx].drug.charAt(0).toUpperCase() +
-          formData[idx].drug.slice(1);
-        drugs.push(drugName);
-        doses.push(formData[idx].dose);
-        frequencies.push(formData[idx].frequency);
+    console.log("getInteractions called");
+    // console.log(formData);
+    const rxCUICodes = [];
+    const drugs = [];
+    const doses = [];
+    const frequencies = [];
+    for (let idx in formData) {
+      rxCUICodes.push(formData[idx].rxCUI);
+      let drugName =
+        formData[idx].drug.charAt(0).toUpperCase() +
+        formData[idx].drug.slice(1);
+      drugs.push(drugName);
+      doses.push(formData[idx].dose);
+      frequencies.push(formData[idx].frequency);
+    }
+    const newInteractions = [];
+    const rxCUIsCode = rxCUICodes.join("+");
+    let interactionURL = `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${rxCUIsCode}`;
+    const response = await axios.get(interactionURL);
+    try {
+      if ("fullInteractionTypeGroup" in response.data) {
+        const interactions = response.data.fullInteractionTypeGroup;
+        // console.log(interactions);
+        // console.log(response.data);
+        newInteractions.push(interactions);
+      } else {
+        const disclaimer = response.data.nlmDisclaimer;
+        // console.log(disclaimer);
+        newInteractions.push([disclaimer]);
       }
-      const newInteractions = [];
-      const rxCUIsCode = rxCUICodes.join("+");
-      let interactionURL = `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${rxCUIsCode}`;
-      const response = await axios.get(interactionURL);
-      try {
-        if ("fullInteractionTypeGroup" in response.data) {
-          const interactions = response.data.fullInteractionTypeGroup;
-          // console.log(interactions);
-          // console.log(response.data);
-          newInteractions.push(interactions);
-        } else {
-          const disclaimer = response.data.nlmDisclaimer;
-          // console.log(disclaimer);
-          newInteractions.push([disclaimer]);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-      const newCurrentData = {
-        rxCUIs: rxCUICodes,
-        interactions: newInteractions,
-        drugs: drugs,
-        doses: doses,
-        frequencies: frequencies,
-      };
-      console.log(newCurrentData);
-      if (type === "interactioncheck") {
-        setCurrentData(newCurrentData);
-      } else if (type === "profileinteractions") {
-        setProfileData(newCurrentData);
-      }
+    } catch (error) {
+      console.log(error);
+    }
+    const newCurrentData = {
+      rxCUIs: rxCUICodes,
+      interactions: newInteractions,
+      drugs: drugs,
+      doses: doses,
+      frequencies: frequencies,
+    };
+    console.log(newCurrentData);
+    if (type === "interactioncheck") {
+      setCurrentData(newCurrentData);
+    } else if (type === "profileinteractions") {
+      setProfileData(newCurrentData);
     }
   };
 
