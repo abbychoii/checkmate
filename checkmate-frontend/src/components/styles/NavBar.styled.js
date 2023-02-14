eb9f26f52ba3c9c705320c193f9bb0b08bce57db
abby <abbychoi97@gmail.com>
2023-02-13 21:11:44 -0800

    updated nav bar links

--- checkmate-frontend/src/components/styles/NavBar.styled.js
+++ checkmate-frontend/src/components/styles/NavBar.styled.js
@@ -3,7 +3,7 @@ export default function Navbar({ user, handleSignOut, setUser }) {
     <div className='min-w-fit max-w-full px-6 bg-white'>
       <div className='flex items-center justify-between border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10'>
         <div className='flex justify-start lg:w-0 lg:flex-1'>
-          <a href='https://localhost:3000' className='bg-transparent'>
+          <a href='https://checkmate.herokuapp.com' className='bg-transparent'>
             <svg
               viewBox='0 0 366 366'
               fill='fill-black'
@@ -18,26 +18,26 @@ export default function Navbar({ user, handleSignOut, setUser }) {
         {/* <div></div> */}
         <div className='flex flex-nowrap items-center justify-end md:flex md:flex-1 lg:w-0 '>
           <a
-            href='http://localhost:3000/'
+            href='https://checkmate.herokuapp.com/'
             className='ml-8 inline-flex items-center justify-center bg-white text-black px-4 py-2 text-base font-medium hover:text-purple-400 hover:-rotate-12 sm:text-sm font-groteskreg lg:text-base'
           >
             Home
           </a>
           <a
-            href='http://localhost:3000/about'
+            href='http://checkmate.herokuapp.com/about'
             className='ml-8 inline-flex items-center justify-center bg-white text-black px-4 py-2 text-base font-medium hover:text-purple-400 hover:rotate-12 sm:text-sm font-groteskreg lg:text-base'
           >
             About
           </a>
           <a
-            href='http://localhost:3000/interactions'
+            href='http://checkmate.herokuapp.com/interactions'
             className='ml-8 inline-flex max-w-[6rem] flex-wrap items-center justify-center bg-white text-black px-4 py-2 text-base font-medium hover:text-purple-400 hover:-rotate-12 sm:text-sm font-groteskreg lg:text-base'
           >
             Interactions CheckMate
           </a>
           {user ? (
             <a
-              href='http://localhost:3000/profile'
+              href='http://checkmate.herokuapp.com/profile'
               className='bg-transparent text-black ml-8 items-center justify-center  hover:rotate-12'
             >
               <img
